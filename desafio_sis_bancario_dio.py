2fb645c0c924e8e7671e7dde5845ad4e33c1c6bb
Victor Weverthon dos Santos Barros Fonsêca Costa <victorweverthonc@gmail.com>
2025-06-10 13:08:52 -0300

    caixa_de_banco_2

--- desafio_sis_bancario_dio.py
+++ desafio_sis_bancario_dio.py
@@ -1,5 +1,7 @@
+import datetime
 import time 
 
+#declaração de variáveis
 saldo = float(0.00)
 limite = 500
 extrato_deposito = float(0.00)
@@ -8,13 +10,17 @@ numero_saques = 0
 limite_saques = 3
 opcao = 0
 
+#Lista para armazenar as transações
+transacoes = []
+
 menu = """
 =========== MENU ===========
 
 [1] Depósito
 [2] Saque
 [3] Extrato
-[4] Sair
+[4] Fazer outra operação
+[5] Sair
 
 =============================
 """
@@ -24,12 +30,25 @@ while True:
     opcao = int(input(menu))
 
     if opcao == 1: 
-        print("Deposito")
+        print("Você selecionou a opção 1 - DEPÓSITO")
         valor = float(input("Valor a ser depositado: R$ "))
-        saldo += valor
-        extrato_deposito += valor
+        if valor <= 0:
+            print("Valor inválido. Por favor, insira um valor positivo.")
+        if valor > 0: 
+            saldo += valor
+            extrato_deposito += valor
+            time.sleep(5)
+            #Registrar a transacao de depósito
+            data_hora = datetime.datetime.now()
+            transacao = {
+                "tipo": "Depósito",
+                "valor": valor,
+                "data_hora": data_hora.strftime("%d/%m/%Y %H:%M:%S")
+            }
+            transacoes.append(transacao)
+            print(f"Depósito de R$ {valor: .2f} realizado com sucesso!!")
     elif opcao == 2:
-        print("Saque")
+        print("Você selecionou a opção 2 - SAQUE")
         valor = float(input("Valor a ser sacado: R$ \n"))
         if saldo < valor: 
             print("Saldo Insuficiente!!!")
@@ -47,16 +66,43 @@ while True:
                 saldo -= valor
                 limite_saques-=1
                 numero_saques += 1
-                extrato_saque -= valor      
+                extrato_saque -= valor
+
+                #Registrar a transação de saque
+                data_hora = datetime.datetime.now()
+                transacao = { 
+                    "tipo": "Saque",
+                    "valor": valor,
+                    "data_hora": data_hora.strftime("%d/%m/%Y %H:%M:%S")
+                }
+                transacoes.append(transacao)
     elif opcao == 3:
-        print("Extrato\n")
+        print("Você selecionou a opção 3 - EXTRATO")
         print(f"Saldo: R$ {saldo: .2f} \n" )
         print("Limite de saque atual: R$ \n", limite_saques)
         print("O número de saques foi de: \n", numero_saques)
-        print(f"Todos os depósitos deste dia foram de: R$ {extrato_deposito: .2f}")
-        print(f"Todos os saques desse dia foram de: R$ {extrato_saque: .2f} ")
+        print("------ Histórico de Transações -------")
+        if not transacoes:
+            print("Nenhuma transação realizada hoje")
+        else:
+            for transacao in transacoes:
+                sinal = " "
+                if transacao["tipo"] == "Saque":
+                    sinal = "-"
+                    print(f"{transacao['data_hora']} | {transacao['tipo']}: {sinal}R$ {transacao['valor']:.2f}")
+
+        print("\n--- Totais Acumulados ---")
+        print(f"Total depositado hoje: R$ {extrato_deposito:.2f}")
+        # Exibe o total de saques como negativo para representação de débito
+        print(f"Total sacado hoje: - R$ {extrato_saque:.2f}")
     elif opcao == 4:
-        print("Obrigado por usar o sistema!!")
+        print("Você selecionou a opção 4 - FAZER OUTRA OPERAÇÃO")
+        print("Retornando ao menu principal...")
+        time.sleep(2)  # Pausa antes de retornar ao menu
+    elif opcao == 5:    
+        print("Você selecionou a opção 5 - SAIR")
+        time.sleep(3)
+        print("Obrigado por usar o Caixa Eletrônico. Até logo!")
         break
     else:
         print("Opção Invalida!! Tente Novamente.")
